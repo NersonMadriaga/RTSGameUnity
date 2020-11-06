b1fc30e974f1ae47d40e704f7894426c454ec0bf
Nerson <nersonmadriaga.taktyl@gmail.com>
2020-11-06 08:49:01 +0800

    Added Resources

---
+++ BuilderDefenderGame/Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour 
+{
+    private Camera mainCamera;
+
+    [SerializeField]
+    private Transform woodHarvester_prefab;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+    // Update is called once per frame
+    private void Update() 
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(woodHarvester_prefab, GetMouseWorldPosition(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0f;
+        return mouseWorldPosition;
+    }
+}
